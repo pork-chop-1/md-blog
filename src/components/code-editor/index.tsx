2c63330e228b5e4c633d530cd08e473489970e2c
Tom <2470017654@qq.com>
2023-11-08 17:02:41 +0800

    ~

---
+++ src/components/code-editor/index.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { useState } from "react"
+
+export default function CodeEditor() {
+  useState()
+
+  return <>
+    <textarea  cols={30} rows={10}></textarea>
+    <div ></div>
+  </>
+}
--- src/components/comment-editor/index.tsx
+++ src/components/comment-editor/index.tsx
@@ -5,8 +5,10 @@ import { useState } from 'react'
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
-import rehypeStringify from 'rehype-stringify'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeSanitize from 'rehype-sanitize'
 import rehypeRaw from 'rehype-raw'
+import rehypeStringify from 'rehype-stringify'
 
 import styles from './index.module.scss'
 
@@ -16,7 +18,9 @@ export default function CommentEditor() {
   const processor = unified()
     .use(remarkParse)
     .use(remarkRehype)
+    .use(rehypeHighlight)
     .use(rehypeRaw) // 处理md mixin html，尤其script
+    .use(rehypeSanitize)
     .use(rehypeStringify)
 
   return (
